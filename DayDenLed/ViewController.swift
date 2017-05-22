8551baf9634d53d77f44f7825369ccb29f80d221
leminhtuan1010 <leminhtuan1010@gmail.com>
2017-05-22 22:05:00 +0700

    first commit

--- DayDenLed/ViewController.swift
+++ DayDenLed/ViewController.swift
@@ -9,15 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txt_nhap: UITextField!
+
+    @IBAction func btn_chay(_ sender: Any) {
+        veBong()
+    }
+    var n: Int!
+    var _margin: CGFloat = 40
+    var bien: CGFloat = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+//    veBong()
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    func veBong()  {
+        n = Int(txt_nhap.text!)
+        for hienThiHang in 0..<n{
+            for hienThiCot in 0..<n{
+           
+            let image = UIImage(named: "bong")
+            let ball = UIImageView(image: image)
+            ball.center = CGPoint(x:_margin + CGFloat(hienThiHang) * tinhkhaongcach(),y:bien + CGFloat(hienThiCot) * chieuDai())
+            self.view.addSubview(ball)
+            
+            }
+        }
+    }
+    func tinhkhaongcach() -> CGFloat {
+        let khoangCach = (self.view.bounds.size.width - (2 * (_margin))) / CGFloat(n - 1)
+        return khoangCach
+    }
+    func chieuDai() -> CGFloat {
+        let khoangCachDai = (self.view.bounds.size.height - (2 * (bien))) / CGFloat(n - 1)
+        return khoangCachDai
     }
 
 
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Bài tập vẽ dãy dèn led và căn chỉnh đều 2 biên, chia đều khoảng cách giữa các quả bóng 
